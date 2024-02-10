02e5d495e3be40e2922574b0f81d173e3073baec
Naman Lalit <namanlalit@Namans-MacBook-Air.local>
2024-02-10 16:47:06 -0500

    Backend APIs

--- src/mongo_scripts/mongo_read.py
+++ src/mongo_scripts/mongo_read.py
@@ -1,4 +1,6 @@
 import pymongo
+from datetime import datetime, timedelta
+from dateutil import parser
 
 class MongoRead:
     def __init__(self, conn_string, db) -> None:
@@ -22,8 +24,36 @@ class MongoRead:
         query = {'CID': id}
         curr = self.db['Companies'].find(query)
         return list(curr)
+    
+    def get_stock_series_for_last_month(self, id, currDate):
+        currDate = parser.parse(currDate)
+        prevDate = currDate - timedelta(days=30)
+        query = {
+            "CID": int(id),
+            "Date": {
+                "$gte": prevDate,
+                "$lte": currDate 
+            }
+        }
+        curr = self.db['StockSeries'].find(query).sort('Date', -1)
+        return list(curr)
 
     def get_news_articles(self, id):
         query = {'CID': id}
         curr = self.db['NewsArticles'].find(query).sort("Date", -1)
+        return list(curr)
+    
+    def get_news_articles_by_goid(self, id):
+        query = {'GOID': int(id)}
+        curr = self.db['NewsArticles'].find(query)
+        return list(curr)
+    
+    def get_news_articles_by_date(self, id):
+        query = {'CID': int(id)}
+        curr = self.db['NewsArticles'].find(query).sort("Date", -1).limit(20)
+        return list(curr)
+    
+    def get_news_articles_by_impact_score(self, id):
+        query = {'CID': int(id)}
+        curr = self.db['NewsArticles'].find(query).sort("impact_score", -1).limit(20)
         return list(curr)
